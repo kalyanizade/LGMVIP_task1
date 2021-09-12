9707b2c9a852367a8756fd0f7f3ca9b08211299f
kalyani zade <87563060+kalyanizade@users.noreply.github.com>
2021-09-12 14:11:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 
-// Select tab content item
 
 function selectItem(e)
- {
-	// Remove all show and border classes
+{
 
 	removeBorder();
 	removeShow();
-
-	// Add border to current tab item
 	this.classList.add('tab-border');
-
-	// Grab content item from DOM
 	const tabContentItem = document.querySelector(`#${this.id}-content`);
-
-	// Add show class
 	tabContentItem.classList.add('show');
 }
 
-// Remove bottom borders from all tab items
 function removeBorder()
  {
 	tabItems.forEach
@@ -28,8 +19,6 @@ function removeBorder()
 		 {item.classList.remove('tab-border');}
 	);
 }
-
-// Remove show class from all content items
 
 function removeShow()
  {
@@ -39,7 +28,6 @@ function removeShow()
 	);
 }
 
-// Listen for tab item click
 tabItems.forEach
 (item =>
 	 {item.addEventListener('click', selectItem);}
